6e622b46d974107c4d276c0cf38253fa981e0b5f
dickyprabowo <dicky.prabowo@mekari.com>
2019-10-29 16:10:56 +0700

    Extract session[:current_email] to application controller

--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -1,8 +1,15 @@
 module Features
     def sign_in
+        # visit root_path
+
+        # fill_in "Email", with: 
+        sign_in_as "person@example.com"
+        # click_on "Sign in"
+    end
+    def sign_in_as(email)
         visit root_path
 
-        fill_in "Email", with: "person@example.com"
+        fill_in "Email", with: email
         click_on "Sign in"
     end
 end
